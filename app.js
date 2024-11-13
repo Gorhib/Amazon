cc3ca3999094ee64be6c7b85dc77cb3cc8986156
Gorhib <98573252+Gorhib@users.noreply.github.com>
2024-11-13 13:43:55 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+//app.js
+
+$(function() {
+  $('#nav-search-select').change(function() {
+    var selectedText = $(this).find('option:selected').text();
+    $('#nav-search').find('.nav-search-label').html(selectedText);
+  });
+});
